2885aea47cc009b9d9cf520bd8e6b68072b905ac
Asylzhan <askobeisembai@gmail.com>
2026-05-29 12:12:59 +0600

    build: добавлен статический экспорт для GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Обязательно для GitHub Pages
+  images: {
+    unoptimized: true, // Отключает оптимизацию картинок, которую GitHub не поддерживает
+  },
+  basePath: '/Smart-queue', // Имя твоего репозитория с точностью до регистра
 };
 
 export default nextConfig;
